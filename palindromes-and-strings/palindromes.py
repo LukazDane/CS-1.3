d0c7d6809649ffad443501da2e3a7d849fb3ba20
Luke <deandrevidal@aol.com>
2020-04-21 13:20:14 -0700

    fixed

--- palindromes-and-strings/palindromes.py
+++ palindromes-and-strings/palindromes.py
@@ -17,14 +17,50 @@ def is_palindrome(text):
 
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
-    pass
+    t = text.lower()
+    left = 0
+    right = len(t) - 1
+    while left < right:
+        if not t[left].isalpha():
+            left += 1
+            continue
+        if not t[right].isalpha():
+            right -= 1
+            continue
+        if t[left] == t[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
 
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
-    pass
+    if left is None and right is None:
+        # initialize left and right indices
+        left = 0
+        right = len(text) - 1
+    if left > right:
+        # Base case: left and right indices have overlapped, text must be a palindrome
+        return True
+
+    t = text.lower()
+
+    if not t[left].isalpha():
+        # left character isn't a letter; move left index to the right by one
+        return is_palindrome_recursive(t, left + 1, right)
+
+    elif not t[right].isalpha():
+        # right character isn't a letter; move right index to the left by one
+        return is_palindrome_recursive(t, left, right - 1)
+
+    elif t[left] == t[right]:
+        return is_palindrome_recursive(t, left + 1, right - 1)
+    else:
+        return False
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
 
